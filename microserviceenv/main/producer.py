26a335de75af8355cf7454cff08459f624c4f41e
rezasblade <rezasblade@gmail.com>
2024-03-04 11:59:05 +0000

    Update producer.py - remove link amqp

--- microserviceenv/main/producer.py
+++ microserviceenv/main/producer.py
@@ -1,6 +1,6 @@
 import pika, json
 
-params = pika.URLParameters('amqps://ohmdljko:h6RD1mcBJtkjCAb0CeYt95cwwWnpwSdB@goose.rmq2.cloudamqp.com/ohmdljko')
+params = pika.URLParameters('link here')
 
 connection = pika.BlockingConnection(params)
 
